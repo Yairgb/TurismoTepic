a4f2c6dfd860d6d34edf0437cdee183315d049f2
Yairgb <ut3ygbaldizon@gmail.com>
2016-09-20 16:29:59 -0600

    Se agregar la activity AR
    Se agrega layout y clase java  donde se ejecutara WIKITUDE

---
+++ app/src/main/java/com/example/ygb/turismotepic/Augmentedreality.java
@@ -0,0 +1,14 @@
+package com.example.ygb.turismotepic;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Augmentedreality extends Activity implements ArchitectViewHolderInterface{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_augmentedreality);
+    }
+}
